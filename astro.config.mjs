c2e2b08b1aa87f5a12d9c7c02e9da27c256d3948
Enzo Damato <edamato@octechservices.org>
2022-09-30 08:17:04 -0400

    many

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,20 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   outDir: './dist',
-  site: 'https://apcsp.io'
+  site: 'https://apcsp.io',
+
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'github-dark',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  }
 });
